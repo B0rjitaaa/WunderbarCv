d86abf39c33d467c2c0a7f55c71a738f1fcc6a9a
Jaretz <bmcx12@gmail.com>
2016-02-13 18:28:09 +0100

    Added static and settings file.

--- WunderbarCv/settings.py
+++ WunderbarCv/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for WunderbarCv project.
+Django settings for wundebareklang project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
@@ -12,19 +12,25 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+IS_ON_OPENSHIFT = False
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    IS_ON_OPENSHIFT = True
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = '0mgs78!a$brv^+z_(9!cl7_cj6n3@ikra!o+)*+3!f_#dqxzma'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if IS_ON_OPENSHIFT:
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+else:
+    DEBUG = True
+    TEMPLATE_DEBUG = True
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cv'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -64,11 +71,10 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+LANGUAGE_CODE = 'es_ES'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -76,8 +82,76 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATE_FORMAT = 'd/m/Y'
+
+DATE_INPUT_FORMATS = ('%d/%m/%Y',)
+DATETIME_INPUT_FORMATS = ('%d/%m/%Y %H:%M',)
+USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
+
+if IS_ON_OPENSHIFT:
+    STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static')
+
+
+if IS_ON_OPENSHIFT:
+    STATICFILES_DIRS = (
+        os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'static'),
+    )
+else:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'), 
+    )
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),  # Needed for the general static files
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+)
+
+# Media files (Uploaded files)
+if IS_ON_OPENSHIFT:
+    MEDIA_URL = '/static/media/'
+    MEDIA_ROOT = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'), 'media')
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Debug Mode in terminal
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    }
+}
+
+for logger in LOGGING['loggers']:
+    LOGGING['loggers'][logger]['handlers'] = ['console']
